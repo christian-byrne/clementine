d7285aa1d642829ba610966c3c09ec9df4f204d5
christian-byrne <abolkonsky.rem@gmail.com>
2024-04-13 20:24:04 -0700

    component loading by breakpoint width on homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,21 +13,25 @@ import sortRecordsByKey from "@/utils/sortRecordsByKey";
 
 function HomePage() {
   useEffect(() => {
-    document.title = "Explore | Featured";
+    document.title = "StyleThis";
   }, []);
 
   const [nCols, setNCols] = useState(6);
+  const [breakpoint, setBreakpoint] = useState("lg");
+
   const adjustNCols = () => {
     const windowWidth = window.innerWidth;
     const breakpoint = getBreakpoint(windowWidth);
     const colSizeMap = {
       sm: 1,
       md: 3,
-      lg: 4,
+      lg: 3,
       xl: 4,
       xxl: 4,
     };
     setNCols(colSizeMap[breakpoint]);
+    setBreakpoint(breakpoint);
+    console.log("Breakpoint: ", breakpoint);
   };
 
   useEffect(() => {
@@ -45,7 +49,13 @@ function HomePage() {
     <MDBContainer fluid>
       <MDBRow className="mt-3">
         {/* Content Rows */}
-        <MDBCol className="col-7 ms-sm-auto pe-2">
+        <MDBCol
+          className={
+            breakpoint == "sm" || breakpoint == "md" || breakpoint == "lg"
+              ? "col-12 ms-sm-auto pe-2"
+              : "col-7 ms-sm-auto pe-2"
+          }
+        >
           <MDBContainer fluid className="mt-4">
             {/* Featured Models Row */}
             <TitleText text="Featured Stylists" />
@@ -55,7 +65,7 @@ function HomePage() {
               sortKey="rating"
               showFirstNCols={nCols}
               maxCols={30}
-              colContainerClass={"col-sm 12 col-md-4 col-lg-3 col-xl-3 mb-4"}
+              colContainerClass={"col-sm 12 col-md-4 col-lg-4 col-xl-3 mb-4 m-xl-0"}
               detailsStartExpanded={false}
             />
             {/* Featured Photos Row */}
@@ -73,41 +83,48 @@ function HomePage() {
           </MDBContainer>
         </MDBCol>
         {/* Leaderboard Preview */}
-        <MDBCol className="col-5 ms-sm-auto">
-          <MDBContainer fluid className="mt-4">
-            {/* <TitleText text="Leaderboards" /> */}
-            <LeaderBoardCard
-              leaderBoardName={"Style Stars"}
-              description={"Users who have the most motion"}
-              leaderBoardData={sortRecordsByKey(allUserData, "views")}
-              visibleColumns={["views"]}
-              maxRows={10}
-              socialBadges={
-                nCols == 1
-                  ? ["likes"]
-                  : nCols < 5
-                  ? ["favorites", "likes"]
-                  : ["downloads", "likes", "favorites"]
-              }
-              containerClassName="col-12 my-3 mx-0 px-0"
-            />
-            <LeaderBoardCard
-              leaderBoardName={"Who to Follow"}
-              description={"Users who have make great content"}
-              leaderBoardData={sortRecordsByKey(allUserData, "modelCount")}
-              visibleColumns={["modelCount"]}
-              maxRows={10}
-              socialBadges={
-                nCols == 1
-                  ? ["avergageRating"]
-                  : nCols < 5
-                  ? ["averageRating", "totalRatings", "downloads"]
-                  : ["averageRating", "totalRatings", "downloads", "favorites"]
-              }
-              containerClassName="col-12 my-3 mx-0 px-0"
-            />
-          </MDBContainer>
-        </MDBCol>
+        {breakpoint != "md" && breakpoint != "sm" && breakpoint != "lg" && (
+          <MDBCol className="col-5 ms-sm-auto">
+            <MDBContainer fluid className="mt-4">
+              {/* <TitleText text="Leaderboards" /> */}
+              <LeaderBoardCard
+                leaderBoardName={"Style Stars"}
+                description={"Users who have the most motion"}
+                leaderBoardData={sortRecordsByKey(allUserData, "views")}
+                visibleColumns={["views"]}
+                maxRows={10}
+                socialBadges={
+                  nCols == 1
+                    ? ["likes"]
+                    : nCols < 5
+                    ? ["favorites", "likes"]
+                    : ["downloads", "likes", "favorites"]
+                }
+                containerClassName="col-12 my-3 mx-0 px-0"
+              />
+              <LeaderBoardCard
+                leaderBoardName={"Who to Follow"}
+                description={"Users who have make great content"}
+                leaderBoardData={sortRecordsByKey(allUserData, "modelCount")}
+                visibleColumns={["modelCount"]}
+                maxRows={10}
+                socialBadges={
+                  nCols == 1
+                    ? ["avergageRating"]
+                    : nCols < 5
+                    ? ["averageRating", "totalRatings", "downloads"]
+                    : [
+                        "averageRating",
+                        "totalRatings",
+                        "downloads",
+                        "favorites",
+                      ]
+                }
+                containerClassName="col-12 my-3 mx-0 px-0"
+              />
+            </MDBContainer>
+          </MDBCol>
+        )}
       </MDBRow>
     </MDBContainer>
   );
