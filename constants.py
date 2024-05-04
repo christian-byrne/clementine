e59c50b1bd512b203bfc46dfaaee86d756d1d35e
christian-byrne <abolkonsky.rem@gmail.com>
2024-05-03 18:25:43 -0700

    Service Classes function call wrapper for handling/logging/catching errors

--- constants.py
+++ constants.py
@@ -1,2 +1,109 @@
 PROJECT_DIR_NAME = "wardrobe"
 BRAND_NAME = "La Vie en Bleu"
+
+# DB Dir Names (relative to data)
+STYLISTS_DB_DIR_NAME = "stylists"
+STYLES_DB_DIR_NAME = "styles"
+USERS_DB_DIR_NAME = "users"
+PHOTOS_DB_DIR_NAME = "photos"
+FILESYSTEM_DB_DIR_NAME = "fs-contents"
+BLOG_DB_DIR_NAME = "blog"
+
+# Public Dir Names (relative to public)
+STYLISTS_PUBLIC_DIR_NAME = "pictures/models"
+STYLES_PUBLIC_DIR_NAME = "pictures/styles"
+USERS_PUBLIC_DIR_NAME = "pictures/pfps"
+SILHOUETTES_PUBLIC_DIR_NAME = "pictures/silhouette-examples"
+BLOG_ASSETS_PUBLIC_DIR_NAME = "pictures/blog-pages"
+BANNERS_PUBLIC_DIR_NAME = "pictures/banner-pics"
+ICONS_PUBLIC_DIR_NAME = "icons"
+VIDS_PUBLIC_DIR_NAME = "videos"
+
+
+VIDEO_EXTENSION_LIST = [
+    ".mp4",
+    ".mov",
+    ".avi",
+    ".mkv",
+    ".webm",
+    ".flv",
+    ".wmv",
+    ".vob",
+    ".ogv",
+    ".ogg",
+    ".m4v",
+    ".m2ts",
+    ".mts",
+    ".m2t",
+    ".ts",
+    ".m2v",
+    ".m2ts",
+    ".m2p",
+    ".m2t",
+    ".m2v",
+    ".m4v",
+    ".m4p",
+    ".m4b",
+    ".m4a",
+    ".m4r",
+    ".m4a",
+    ".m4b",
+    ".m4r",
+    ".m4p",
+    ".3gp",
+    ".3g2",
+    ".3gpp",
+]
+PICTURE_EXTENSION_LIST = [
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".tiff",
+    ".bmp",
+    ".webp",
+    ".svg",
+    ".eps",
+    ".raw",
+    ".cr2",
+    ".nef",
+    ".orf",
+    ".sr2",
+    ".raf",
+    ".pef",
+    ".dng",
+    ".ico",
+    ".psd",
+    ".ai",
+    ".xcf",
+    ".indd",
+    ".cdr",
+    ".xpm",
+    ".dds",
+    ".tga",
+    ".thm",
+    ".yuv",
+]
+TEXT_EXTENSION_LIST = [
+    ".txt",
+    ".json",
+    ".csv",
+    ".xml",
+    ".yaml",
+    ".yml",
+    ".html",
+    ".md",
+    ".rst",
+    ".log",
+    ".pdf",
+    ".doc",
+    ".docx",
+    ".odt",
+    ".rtf",
+    ".tex",
+    ".wik",
+    ".ini",
+    ".conf",
+    ".cfg",
+    ".less",
+]
